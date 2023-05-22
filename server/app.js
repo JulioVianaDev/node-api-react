2a9247245154dc3e7aa9bcc4eec9d5a1270d26e9
juliovianadev <juliovianadev@gmail.com>
2023-05-22 11:01:54 -0300

    conseguindo criar post pelo insomnia

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const feedRoutes = require('./routes/feed')
+const bodyParser = require('body-parser')
 const app = express();
-
+// app.use(bodyParser.urlencoded());//para formulários
 
 // GET /feed/posts
+app.use(bodyParser.json())
 app.use('/feed',feedRoutes);
 
-app.listen(8080)
+app.listen(4000)
--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,3 +1,14 @@
 exports.getPosts = (req,res,next)=>{
-    
+    res.status(200).json({
+        posts: [{title: "primeiro post",content: "meu primeiro post"}]
+    })
+}
+
+exports.createPost= (req,res,next)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({
+        message: "Post criado com sucesso",
+        post: {id: new Date().toISOString(),title: title,content: content}
+    })
 }
